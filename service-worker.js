73e91710ef3220288f0a53a6ecb6cfe65d840f14
Arduino15650 <109769427+Arduino15650@users.noreply.github.com>
2026-09-22 23:49:22 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bac-pro-melec-v55-visual-results';
+const CACHE_NAME = 'bac-pro-melec-v56-horizontal-navigation';
 const APP_FILES = [
   './','./index.html','./styles.css','./bo.css','./referential.js','./app.js',
   './performance.js','./workshop-activities-v2.js','./evaluation-atelier-v2.js','./home.js','./results.js','./pdf-generator-v2.js',
